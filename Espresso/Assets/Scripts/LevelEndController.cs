3fcd88b68046aac5da039918e00a4614eb8b0068
gongbaodd@ProArt <gongbaodd@qq.com>
2025-03-29 11:11:23 +0200

    Enhance LevelEndController to dynamically load scenes based on current level

--- Espresso/Assets/Scripts/LevelEndController.cs
+++ Espresso/Assets/Scripts/LevelEndController.cs
@@ -4,12 +4,21 @@ using UnityEngine.SceneManagement;
 
 public class LevelEndController : MonoBehaviour
 {
+    [SerializeField] private string levelName = "Level_with_stuff";
+    [SerializeField] private string levelEnd = "LevelEnd";
     void OnTriggerEnter2D(Collider2D collision)
     {
         print("Collision detected with: " + collision.gameObject.name);
         if (collision.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene("LevelEnd");
+            if (SceneManager.GetActiveScene().name == levelName)
+            {
+                SceneManager.LoadScene(levelEnd, LoadSceneMode.Single);
+            }
+            else
+            {
+                SceneManager.LoadScene(levelName, LoadSceneMode.Single);
+            }
         }
     }
 }
