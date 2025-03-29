5dc67fa987798c255aad2e5ebd6dc4cc98054b0f
gongbaodd@ProArt <gongbaodd@qq.com>
2025-03-29 17:28:02 +0200

    Refactor LevelEndController to simplify scene loading logic

--- Espresso/Assets/Scripts/LevelEndController.cs
+++ Espresso/Assets/Scripts/LevelEndController.cs
@@ -4,21 +4,13 @@ using UnityEngine.SceneManagement;
 
 public class LevelEndController : MonoBehaviour
 {
-    [SerializeField] private string levelName = "Level_with_stuff";
-    [SerializeField] private string levelEnd = "LevelEnd";
+    [SerializeField] private string levelName = "LevelMenu";
     void OnTriggerEnter2D(Collider2D collision)
     {
         print("Collision detected with: " + collision.gameObject.name);
         if (collision.gameObject.CompareTag("Player"))
         {
-            if (SceneManager.GetActiveScene().name == levelName)
-            {
-                SceneManager.LoadScene(levelEnd, LoadSceneMode.Single);
-            }
-            else
-            {
-                SceneManager.LoadScene(levelName, LoadSceneMode.Single);
-            }
+            SceneManager.LoadScene(levelName, LoadSceneMode.Single);
         }
     }
 }
