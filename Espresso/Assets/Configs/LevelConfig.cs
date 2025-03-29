af9f4673b9c63493053cc87543b58338c731e044
gongbaodd@ProArt <gongbaodd@qq.com>
2025-03-29 13:34:31 +0200

    Add TemperatureController and update LevelConfig for temperature management

--- Espresso/Assets/Configs/LevelConfig.cs
+++ Espresso/Assets/Configs/LevelConfig.cs
@@ -5,6 +5,7 @@ public class LevelConfig : ScriptableObject
 {
     public float runningSpeed = 2f;
     public float jumpForce = 5f;
-
     public float gravity = 9.81f; // Default gravity value
+
+    public float temperatureUpdateInterval = .5f; // Interval in seconds for temperature updates
 }
--- Espresso/Assets/Scripts/TemperatureController.cs
+++ Espresso/Assets/Scripts/TemperatureController.cs
@@ -1,6 +1,59 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UIElements;
 
+[RequireComponent(typeof(UIDocument))]
 public class TemperatureController : MonoBehaviour
 {
     public float currentTemperature = 50f;
+
+    private GameObject player;
+
+    private GameObject manager;
+
+    private ProgressBar progressBar;
+
+    void Start()
+    {
+        var uiDocument = GetComponent<UIDocument>();
+        progressBar = uiDocument.rootVisualElement.Q<ProgressBar>("ProgressBar");
+
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player == null)
+        {
+            throw new System.Exception("Player GameObject not found. Make sure it has the 'Player' tag.");
+        }
+
+        manager = GameObject.FindGameObjectWithTag("Manager");
+
+        if (manager == null)
+        {
+            throw new System.Exception("Manager GameObject not found. Make sure it has the 'Manager' tag.");
+        }
+
+        StartCoroutine(UpdateTemperature());
+
+    }
+
+
+    IEnumerator UpdateTemperature()
+    {
+        var playerController = player.GetComponent<PlayerController>();
+        var interval = manager.GetComponent<LevelManagerController>().levelConfig.temperatureUpdateInterval;
+
+        while(true) {
+            yield return new WaitForSeconds(interval);
+            if (playerController.isDown && currentTemperature < 100f)
+            {
+                currentTemperature += 1f;
+                progressBar.value = currentTemperature;
+            }
+            else if (!playerController.isDown && currentTemperature > 0f)
+            {
+                currentTemperature -= 1f;
+                progressBar.value = currentTemperature;
+            }
+        }
+    }
 }
